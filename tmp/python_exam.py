1ae9373b0550c37ad0c5670d6a07bda4916221f6
x7852398521 <x7852398521@hotmail.com>
2020-08-05 17:01:29 +0800

    ok

--- tmp/python_exam.py
+++ tmp/python_exam.py
@@ -1,42 +1,20 @@
 # -*- coding: utf-8 -*-
 
-input_value = {
-  'hired': {
-    'be': {
-      'to': {
-        'deserve': 'I'
-      }
-    }
-  }
-}
+def dict_reverse(input_value):
+    L=[]
+    while input_value.keys():
+        L.append(list(input_value.keys())[0])  
+        input_value = input_value[list(input_value.keys())[0]]
+        if type(input_value) != dict:
+            L.append(input_value)
+            break
+    k,v = L[1:],L[0]
+    for i in k:
+        D = dict()
+        D[i] = v
+        v = D
+    return v
 
-
-L=[]
-while input_value.keys():
-    L.append(list(input_value.keys())[0])  
-    input_value = input_value[list(input_value.keys())[0]]
-    if type(input_value) != dict:
-        L.append(input_value)
-        break
-k,v = L[1:],L[0]
-for i in k:
-    D = dict()
-    D[i] = v
-    v = D
-
-print(v)
-
-"""輸出結果
-output_value = {
-  'I': {
-    'deserve': {
-      'to': {
-         'be': 'hired'
-      }
-    }
-  }
-}
-"""
 
 
 
